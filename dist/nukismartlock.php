b5530a70c586ae8a48be58e52b764e3be6bebfa7
jluc2808 <jean-luc.berno@wanadoo.fr>
2021-10-10 09:53:43 +0200

    Update nukismartlock.php
    update according with v1.4

--- dist/nukismartlock.php
+++ dist/nukismartlock.php
@@ -2,17 +2,23 @@
 /** ****************************************************************************
 * Nikya eedomus Script Nuki Smartlock
 ********************************************************************************
-* Plugin version : 1.3
+* Plugin version : 1.4
 * Author : Nikya
+* Mod: jluc2808
 * Origine : https://github.com/Nikya/eedomusScript_nuki_smartlock
+* Fork: https://github.com/jluc2808/eedomusScript_nuki_smartlock
 * Nuki Bridge HTTP-API : 1.6
+
+* contents 1.4
+* Add door state
+* Mod battery average instead of state only
 *******************************************************************************/
 
 /** Utile en cours de dev uniquement */
 //$eedomusScriptsEmulatorDatasetPath = "eedomusScriptsEmulator_dataset.json";
 //require_once ("eedomusScriptsEmulator.php");
 
-/** Initialisation de la réponse */
+/** Initialisation de la rÃ©ponse */
 $response = null;
 
 /** Lecture de la fonction */
@@ -26,7 +32,7 @@ switch($function) {
 		sdk_setup(getArg('nukihost_port'), getArg('token'));
 		break;
 	case 'register':
-		sdk_register(getArg('eedomushost'), getArg('nukiid'), getArg('periph_id_state'), getArg('periph_id_batterycritical'));
+		sdk_register(getArg('eedomushost'), getArg('nukiid'), getArg('periph_id_state'), getArg('periph_id_batterycritical'), getArg('periph_id_doorstate'));
 		break;
 	case 'list':
 		sdk_callAPI('list');
@@ -46,7 +52,7 @@ switch($function) {
 
 /** ****************************************************************************
 * Enregister les informations pour communiquer avec le Bridge Nuki et affiche
-* la liste des serrures connues sur le pont ciblé.
+* la liste des serrures connues sur le pont ciblÃ©.
 *
 * @param $nukihost Host IP du Nuki
 * @param $nukiport Port du Nuki
@@ -61,10 +67,10 @@ function sdk_setup($nukihost_port, $token) {
 
 /** ****************************************************************************
 * Enregister les informations
-* - Côté eedomus : Les id des 2 périphériques d'informations
-* - Côté Nuki : Enregistre ce script en tant que callBack
+* - CÃ´tÃ© eedomus : Les id des 4 pÃ©riphÃ©riques d'informations
+* - CÃ´tÃ© Nuki : Enregistre ce script en tant que callBack
 */
-function sdk_register($eedomushost, $nukiid, $periph_id_state, $periph_id_batterycritical) {
+function sdk_register($eedomushost, $nukiid, $periph_id_state, $periph_id_batterycritical, $periph_id_doorstate) {
 	global $response;
 
 	$eScript = explode( '/' , __FILE__);
@@ -80,46 +86,54 @@ function sdk_register($eedomushost, $nukiid, $periph_id_state, $periph_id_batter
 	saveVariable('nukiid', $nukiid);
 	saveVariable("periph_id_state$nukiid", $periph_id_state);
 	saveVariable("periph_id_batterycritical$nukiid", $periph_id_batterycritical);
+	saveVariable("periph_id_doorstate$nukiid", $periph_id_doorstate);
 
 	sdk_callAPI('callback/add', array('url' => $fullUrl));
 }
 
 /** ****************************************************************************
-* Fonction appelée par un callback de la part de Nuki.
-* Est rappeler à chaque changement d'état.
+* Fonction appelÃ©e par un callback de la part de Nuki.
+* Est rappeler Ã  chaque changement d'Ã©tat.
 */
 function sdk_incomingCall() {
 	global $response;
 
-	// Le callback est accompagné d'un Json contenant les nouvelles valeurs
+	// Le callback est accompagnÃ© d'un Json contenant les nouvelles valeurs
 	//		{"nukiId": 11, "state": 1, "stateName": "locked", "batteryCritical": false}
 	$backData = sdk_json_decode(sdk_get_input());
 	$nukiid = $backData['nukiId'];
 	$periph_value_state = $backData['state'];
-	$periph_value_batterycritical = $backData['batteryCritical'];
+	// $periph_value_batterycritical = $backData['batteryCritical'];  - removed old value
+	$periph_value_batterychargestate = $backData['batteryChargeState'];
+	$periph_value_doorstate = $backData['doorsensorState'];
 
 	$periph_id_state = loadVariable("periph_id_state$nukiid");
 	$periph_id_batterycritical = loadVariable("periph_id_batterycritical$nukiid");
+	$periph_id_doorstate = loadVariable("periph_id_doorstate$nukiid");
 
 	setValue($periph_id_state, $periph_value_state);
-	setValue($periph_id_batterycritical, $periph_value_batterycritical);
+	// setValue($periph_id_batterycritical, $periph_value_batterycritical); - removed old value
+	setValue($periph_id_batterycritical, $periph_value_batterychargestate);
+	setValue($periph_id_doorstate, $periph_value_doorstate);
 
 	$response = ' { ';
 	$response.= ' "nukiid" : "'. $nukiid .'", ';
 	$response.= ' "periph_id_state" : "'. $periph_id_state .'", ';
 	$response.= ' "periph_id_batterycritical" : "'. $periph_id_batterycritical .'", ';
 	$response.= ' "periph_value_state" : "'. $periph_value_state .'", ';
-	$response.= ' "periph_value_batterycritical" : "'. $periph_value_batterycritical .'" ';
+//	$response.= ' "periph_value_batterycritical" : "'. $periph_value_batterycritical .'" '; - removed old value
+	$response.= ' "periph_value_batterychargestate" : "'. $periph_value_batterychargestate .'" ';
+	$response.= ' "periph_value_doorstate" : "'. $periph_value_doorstate .'" ';
 	$response.= ' } ';
 }
 
 /** ****************************************************************************
 * Appeler l'API de Nuki
 *
-* @param $endpoint Endpoint ciblé
-* @param $params Tableau de paramétre à envoyer sur la cible
+* @param $endpoint Endpoint ciblÃ©
+* @param $params Tableau de paramÃ©tre Ã  envoyer sur la cible
 *
-* @return le résulat de l'appel au format Json
+* @return le rÃ©sulat de l'appel au format Json
 */
 function sdk_callAPI($endpoint, $params=array()) {
 	global $response;
@@ -141,7 +155,8 @@ function sdk_callAPI($endpoint, $params=array()) {
 }
 
 /** ****************************************************************************
-* Fin du script, affichage du résultat au format XML
+* Fin du script, affichage du rÃ©sultat au format XML
 */
 sdk_header('text/xml');
 echo jsonToXML($response);
+?>
